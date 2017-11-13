0d059cb21c36bc653b67701f92da90b14d9955ad
Filipe Rodrigues <rodr@dtu.dk>
2017-11-13 13:58:32 +0100

    updated code for compatibility with keras 2.08

--- crowd_layer/crowd_aggregators.py
+++ crowd_layer/crowd_aggregators.py
@@ -132,4 +132,66 @@ class CrowdsCategoricalAggregator():
 		return self.model, self.pi
 
 
+class CrowdsSequenceAggregator():
+
+        def __init__(self, model, data_train, answers, batch_size = 16, pi_prior = 1.0):
+                self.model = model
+                self.data_train = data_train
+                self.answers = answers
+                self.batch_size = batch_size
+                self.pi_prior = pi_prior
+                self.n_train = answers.shape[0]
+                self.seq_length = answers.shape[1]
+                self.num_classes = np.max(answers) + 1
+                self.num_annotators = answers.shape[2]
+                print "n_train:", self.n_train
+                print "seq_length:", self.seq_length
+                print "num_annotators:", self.num_annotators
+
+                # initialize annotators as reliable (almost perfect)
+                self.pi = self.pi_prior * np.ones((self.num_classes,self.num_classes,self.num_annotators))
+
+                # initialize estimated ground truth with majority voting
+                self.ground_truth_est = np.zeros((self.n_train, self.seq_length, self.num_classes))
+                for i in xrange(self.n_train):
+                        for j in xrange(self.seq_length):
+                                votes = np.zeros(self.num_annotators)
+                                for r in xrange(self.num_annotators):
+                                        if answers[i,j,r] != -1:
+                                                votes[answers[i,j,r]] += 1
+                                self.ground_truth_est[i,j,np.argmax(votes)] = 1.0
+
+
+        def e_step(self):
+                print "E-step"
+                for i in xrange(self.n_train):
+                        for j in xrange(self.seq_length):
+                                adjustment_factor = np.ones(self.num_classes)
+                                for r in xrange(self.num_annotators):
+                                        if self.answers[i,j,r] != -1:
+                                                adjustment_factor *= self.pi[:,self.answers[i,j,r],r]
+                                self.ground_truth_est[i,j,:] = np.transpose(adjustment_factor) * self.ground_truth_est[i,j,:]
+
+                return self.ground_truth_est
+
+
+        def m_step(self,epochs):
+                print "M-step"
+                hist = self.model.fit(self.data_train, self.ground_truth_est, epochs=epochs, shuffle=True, batch_size=self.batch_size, verbose=0)
+                print "loss:", hist.history["loss"][-1]
+                self.ground_truth_est = self.model.predict(self.data_train)
+
+                self.pi = self.pi_prior * np.ones((self.num_classes,self.num_classes,self.num_annotators))
+                for r in xrange(self.num_annotators):
+                        normalizer = np.zeros(self.num_classes)
+                        for i in xrange(self.n_train):
+                                for j in xrange(self.seq_length):
+                                        if self.answers[i,j,r] != -1:
+                                                self.pi[:,self.answers[i,j,r],r] += np.transpose(self.ground_truth_est[i,j,:])
+                                                normalizer += self.ground_truth_est[i,j,:]
+                        normalizer = np.expand_dims(normalizer, axis=1)
+                        self.pi[:,:,r] = self.pi[:,:,r] / np.tile(normalizer, [1, self.num_classes])
+
+                return self.model, self.pi
+
 		
--- crowd_layer/crowd_layers.py
+++ crowd_layer/crowd_layers.py
@@ -23,26 +23,32 @@ class CrowdsClassification(Layer):
 	def build(self, input_shape):
 		if self.conn_type == "MW":
 			# matrix of weights per annotator
-			self.kernel = self.add_weight(shape=(self.output_dim, self.output_dim, self.num_annotators),
+			self.kernel = self.add_weight("CrowdLayer", (self.output_dim, self.output_dim, self.num_annotators),
 											initializer=init_identities, 
 											trainable=True)
 		elif self.conn_type == "VW":
 			# vector of weights (one scale per class) per annotator
-			self.kernel = self.add_weight(shape=(self.output_dim, self.num_annotators),
+			self.kernel = self.add_weight("CrowdLayer", (self.output_dim, self.num_annotators),
 											initializer=keras.initializers.Ones(), 
 											trainable=True)
+		elif self.conn_type == "VB":
+			# two vectors of weights (one scale and one bias per class) per annotator
+			self.kernel = []
+			self.kernel.append(self.add_weight("CrowdLayer", (self.output_dim, self.num_annotators),
+											initializer=keras.initializers.Zeros(),
+											trainable=True))
 		elif self.conn_type == "VW+B":
 			# two vectors of weights (one scale and one bias per class) per annotator
 			self.kernel = []
-			self.kernel.append(self.add_weight(shape=(self.output_dim, self.num_annotators),
+			self.kernel.append(self.add_weight("CrowdLayer", (self.output_dim, self.num_annotators),
 											initializer=keras.initializers.Ones(),
 											trainable=True))
-			self.kernel.append(self.add_weight(shape=(self.output_dim, self.num_annotators),
+			self.kernel.append(self.add_weight("CrowdLayer", (self.output_dim, self.num_annotators),
 											initializer=keras.initializers.Zeros(),
 											trainable=True))
 		elif self.conn_type == "SW":
 			# single weight value per annotator
-			self.kernel = self.add_weight(shape=(self.num_annotators,1),
+			self.kernel = self.add_weight("CrowdLayer", (self.num_annotators,1),
 											initializer=keras.initializers.Ones(),
 											trainable=True)
 		else:
@@ -53,11 +59,13 @@ class CrowdsClassification(Layer):
 	def call(self, x):
 		if self.conn_type == "MW":
 			res = K.dot(x, self.kernel)
-		elif self.conn_type == "VW" or self.conn_type == "VW+B" or self.conn_type == "SW":
+		elif self.conn_type == "VW" or self.conn_type == "VB" or self.conn_type == "VW+B" or self.conn_type == "SW":
 			out = []
 			for r in range(self.num_annotators):
 				if self.conn_type == "VW":
 					out.append(x * self.kernel[:,r])
+				elif self.conn_type == "VB":
+					out.append(x + self.kernel[0][:,r])
 				elif self.conn_type == "VW+B":
 					out.append(x * self.kernel[0][:,r] + self.kernel[1][:,r])
 				elif self.conn_type == "SW":
@@ -89,20 +97,20 @@ class CrowdsRegression(Layer):
 		self.kernel = []
 		if self.conn_type == "S":
 			# scale-only parameter
-			self.kernel.append(self.add_weight(shape=(1, self.num_annotators),
+			self.kernel.append(self.add_weight("CrowdLayer", (1, self.num_annotators),
 								  initializer=keras.initializers.Ones(),
 								  trainable=True))
 		elif self.conn_type == "B":
 			# bias-only parameter
-			self.kernel.append(self.add_weight(shape=(1, self.num_annotators),
+			self.kernel.append(self.add_weight("CrowdLayer", (1, self.num_annotators),
 								  initializer=keras.initializers.Zeros(),
 								  trainable=True))
 		elif self.conn_type == "S+B" or self.conn_type == "B+S":
 			# scale and bias parameters
-			self.kernel.append(self.add_weight(shape=(1, self.num_annotators),
+			self.kernel.append(self.add_weight("CrowdLayer", (1, self.num_annotators),
 									  initializer=keras.initializers.Ones(),
 									  trainable=True))
-			self.kernel.append(self.add_weight(shape=(1, self.num_annotators),
+			self.kernel.append(self.add_weight("CrowdLayer", (1, self.num_annotators),
 									  initializer=keras.initializers.Zeros(),
 									  trainable=True))
 		else:
@@ -169,5 +177,185 @@ class MaskedMultiSequenceCrossEntropy(object):
 		vec = tf.where(mask_padding, x=zer, y=vec)
 		loss = tf.reduce_mean(vec, axis=-1)
 		return loss
+
+
+class CrowdsAggregationCategoricalCrossEntropy(object):
+
+	def __init__(self, num_classes, num_annotators, pi_prior=0.01):
+		self.num_classes = num_classes
+		self.num_annotators = num_annotators
+		self.pi_prior = pi_prior
+		
+		# initialize pi_est (annotators' estimated confusion matrices) wit identities
+		self.pi_est = np.zeros((self.num_classes,self.num_classes,self.num_annotators), dtype=np.float32)
+		for r in xrange(self.num_annotators):
+			self.pi_est[:,:,r] = np.eye(self.num_classes) + self.pi_prior
+			self.pi_est[:,:,r] /= np.sum(self.pi_est[:,:,r], axis=1)
+			
+		self.init_suff_stats()
+			
+	def init_suff_stats(self):
+		# initialize suff stats for M-step
+		self.suff_stats = self.pi_prior * tf.ones((self.num_annotators,self.num_classes,self.num_classes))
+		
+	def loss_fc(self, y_true, y_pred):
+		y_true = tf.cast(y_true, tf.int32)
+
+		#y_pred += 0.01
+		#y_pred /= tf.reduce_sum(y_pred, reduction_indices=len(y_pred.get_shape()) - 1, keep_dims=True)
+
+		#y_pred = tf.where(tf.less(y_pred, 0.001), 
+		#                        #0.01 * tf.ones_like(y_pred), 
+		#                        0.001 + y_pred, 
+		#                        y_pred)
+		#y_pred += 0.01 # y_pred cannot be zero!
+		eps = 1e-3
+		#y_pred = tf.clip_by_value(y_pred, eps, 1.0 - eps)
+		y_pred = tf.clip_by_value(y_pred, eps, 9999999999)
+
+
+		# E-step
+		adjustment_factor = tf.ones_like(y_pred)
+		for r in xrange(self.num_annotators):
+			adj = tf.where(tf.equal(y_true[:,r], -1), 
+								tf.ones_like(y_pred), 
+								tf.gather(tf.transpose(self.pi_est[:,:,r]), y_true[:,r]))
+			adjustment_factor = tf.multiply(adjustment_factor, adj)
+			
+		res = tf.multiply(adjustment_factor, y_pred)
+		y_agg = res / tf.expand_dims(tf.reduce_sum(res, axis=1), 1)
+
+		loss = -tf.reduce_sum(y_agg * tf.log(y_pred), reduction_indices=[1])
+		
+		# update suff stats
+		upd_suff_stats = []
+		for r in xrange(self.num_annotators):
+			#print r
+			suff_stats = []
+			normalizer = tf.zeros_like(y_pred)
+			for c in xrange(self.num_classes):
+				suff_stats.append(tf.reduce_sum(tf.where(tf.equal(y_true[:,r], c), 
+									y_agg,
+									tf.zeros_like(y_pred)), axis=0))
+			upd_suff_stats.append(suff_stats)
+		upd_suff_stats = tf.stack(upd_suff_stats)
+		self.suff_stats += upd_suff_stats
+
+		return loss
+	
+	def m_step(self):
+		#print "M-step"
+		self.pi_est = tf.transpose(self.suff_stats / tf.expand_dims(tf.reduce_sum(self.suff_stats, axis=2), 2), [1, 2, 0])
+		
+		return self.pi_est
+
+
+class CrowdsAggregationBinaryCrossEntropy(object):
+
+	def __init__(self, num_annotators, pi_prior=0.01, alpha=None, beta=None, update_freq=1):
+		self.num_annotators = num_annotators
+		self.pi_prior = pi_prior
+		self.alpha = alpha
+		self.beta = beta
+		self.update_freq = update_freq
+		
+		# initialize alpha and beta (annotators' estimated sensitivity and specificity)
+		if self.alpha == None:
+			print "initializing alpha with unit..."
+			self.alpha = 0.99*np.ones((self.num_annotators,1), dtype=np.float32)
+		if self.beta == None:
+			self.beta = 0.99*np.ones((self.num_annotators,1), dtype=np.float32)
+		self.count = tf.ones(1)
+			
+		self.suff_stats_alpha = [self.pi_prior for r in xrange(self.num_annotators)]
+		self.suff_stats_beta = [self.pi_prior for r in xrange(self.num_annotators)]
+		self.suff_stats_alpha_norm = [self.pi_prior for r in xrange(self.num_annotators)]
+		self.suff_stats_beta_norm = [self.pi_prior for r in xrange(self.num_annotators)]
+			
+	def init_suff_stats(self):
+		# initialize suff stats for M-step
+		pass
+
+	def loss_fc(self, y_true, y_pred):
+		#y_true = tf.cast(y_true, tf.int32)
+
+		#y_pred += 0.01
+		#y_pred /= tf.reduce_sum(y_pred, reduction_indices=len(y_pred.get_shape()) - 1, keep_dims=True)
+
+		#y_pred = tf.where(tf.less(y_pred, 0.001), 
+		#                        #0.01 * tf.ones_like(y_pred), 
+		#                        0.001 + y_pred, 
+		#                        y_pred)
+		#y_pred += 0.01 # y_pred cannot be zero!
+		eps = 1e-3
+		y_pred = tf.clip_by_value(y_pred, eps, 1.0 - eps)
+		#y_pred = tf.clip_by_value(y_pred, eps, 9999999999)
+
+		p = y_pred[:,1]
+		self.count += 1
+		self.count = tf.Print(self.count, [self.count])
+		#self.count += 1
+
+		if False:
+			print "M-step..."
+			self.alpha = []
+			self.beta = []
+			for r in xrange(self.num_annotators):
+				self.alpha.append(self.suff_stats_alpha[r] / self.suff_stats_alpha_norm[r])
+				self.beta.append(self.suff_stats_beta[r] / self.suff_stats_beta_norm[r])
+			self.count = 0
+			self.suff_stats_alpha = [self.pi_prior for r in xrange(self.num_annotators)]
+			self.suff_stats_beta = [self.pi_prior for r in xrange(self.num_annotators)]
+			self.suff_stats_alpha_norm = [self.pi_prior for r in xrange(self.num_annotators)]
+			self.suff_stats_beta_norm = [self.pi_prior for r in xrange(self.num_annotators)]
+			self.alpha = tf.Print(self.alpha, [self.alpha])
+
+		
+		# E-step
+		a = tf.ones_like(p)
+		b = tf.ones_like(p)
+		for r in xrange(self.num_annotators):
+			a = a * tf.where(tf.equal(y_true[:,r], 1), self.alpha[r]*tf.ones_like(p), tf.ones_like(p))
+			b = b * tf.where(tf.equal(y_true[:,r], 1), (1.0-self.beta[r])*tf.ones_like(p), tf.ones_like(p))
+			a = a * tf.where(tf.equal(y_true[:,r], 0), (1.0-self.alpha[r])*tf.ones_like(p), tf.ones_like(p))
+			b = b * tf.where(tf.equal(y_true[:,r], 0), self.beta[r]*tf.ones_like(p), tf.ones_like(p))
+		
+		mu = (a*p) / (a*p + b*(1.0-p))
+		#mu = tf.Print(mu, [mu])
+		loss = - (mu * tf.log(y_pred[:,1]) + (1.0-mu) * tf.log(y_pred[:,0]))
+
+		# update suff stats
+		for r in xrange(self.num_annotators):
+			self.suff_stats_alpha[r] += tf.reduce_sum(tf.where(tf.equal(y_true[:,r], 1), mu, tf.zeros_like(p)))
+			self.suff_stats_beta[r] += tf.reduce_sum(tf.where(tf.equal(y_true[:,r], 0), (1.0-mu), tf.zeros_like(p)))
+			self.suff_stats_alpha_norm[r] += tf.reduce_sum(tf.where(tf.equal(y_true[:,r], -1), tf.zeros_like(p), mu))
+			self.suff_stats_beta_norm[r] += tf.reduce_sum(tf.where(tf.equal(y_true[:,r], -1), tf.zeros_like(p), (1.0-mu)))
+		
+		return loss
+	
+	def m_step(self):
+		print dir(self)
+		print "debug:", self.count.eval()
+		#print "M-step"
+		#self.count += 1
+		#print "increment", self.count
+		#if self.count >= self.update_freq:
+			
+			
+		
+		return (self.alpha, self.beta)
+
+
+class CrowdsAggregationCallback(keras.callbacks.Callback):
+
+	def __init__(self, loss):
+		self.loss = loss
+		
+	def on_epoch_begin(self, epoch, logs=None):
+		self.loss.init_suff_stats()
+		
+	def on_epoch_end(self, epoch, logs=None):
+		# run M-step
+		self.model.pi = self.loss.m_step()
 
 
